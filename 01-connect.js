51c5dbc47c4959b0785c092e21eada7ac8c8faad
Poly Titova <poly_titova@mail.ru>
2021-04-08 21:47:18 +0300

    Выполнили запросы

--- 01-connect.js
+++ 01-connect.js
@@ -11,3 +11,14 @@ client.connect((err) => {
 
   console.log(`Соединение с PostgreSQL установлено!`);
 });
+
+// получение всех записей таблицы
+const sql = `SELECT * FROM peoples`;
+client.query(sql, (err, res) => {
+  if (err) {
+    throw new Error(`Ошибка при выполнении запроса: ${err.message}`);
+  }
+
+  console.table(res.rows);
+  client.end();
+});
