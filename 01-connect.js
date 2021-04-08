ab1b6d87d8aa0513dc5cf8a1ddbf52f359d9305c
Poly Titova <poly_titova@mail.ru>
2021-04-08 21:43:15 +0300

    Синициировали процесс подключения

--- 01-connect.js
+++ 01-connect.js
@@ -1,3 +1,13 @@
 // подключение пакета
 const {Client} = require(`pg`);
 const connectionString = `postgres://academy:123456@localhost/phonebook`;
+
+// проверка на ошибку
+client.connect((err) => {
+
+  if (err) {
+    throw new Error(`Ошибка установки соединения: ${err.message}`);
+  }
+
+  console.log(`Соединение с PostgreSQL установлено!`);
+});
