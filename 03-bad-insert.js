ba25eea97fb09a2df5a5ac959dd48c806236bd4e
Poly Titova <poly_titova@mail.ru>
2021-04-09 17:34:55 +0300

    Добавили в таблицу информацию о новом абоненте

---
+++ 03-bad-insert.js
@@ -0,0 +1,22 @@
+'use strict';
+
+// подключение пакета
+const {Client} = require(`pg`);
+
+(async () => {
+  const connectionString = `postgres://academy:123456@localhost/phonebook`;
+
+  const firstname = `Axl`;
+  const lastname = `Rose`;
+  const birthDate = `1962-02-06`;
+  const email = `test@gr.local`;
+
+  const insertQuery = `INSERT INTO peoples
+  VALUES(DEFAULT, '${firstname}', '${lastname}', '${birthDate}', '${email}')`;
+
+  const client = new Client(connectionString);
+  await client.connect();
+  await client.query(insertQuery);
+
+  client.end();
+})();
