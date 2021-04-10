41ec03578e4b2baf6b7e8391afddce9d8d23e06c
Poly Titova <poly_titova@mail.ru>
2021-04-10 11:02:23 +0300

    Сделали пул соединение

---
+++ 06-pool.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const {Pool} = require(`pg`);
+
+const pool = new Pool({
+  host: `localhost`,
+  port: 5432,
+  database: `phonebook`,
+  user: `academy`,
+  password: `123456`,
+});
+
+(async () => {
+  const client = await pool.connect();
+  const sql = `SELECT * FROM peoples`;
+  const {rows} = await client.query(sql);
+
+  // Возвращаем в pool
+  client.release();
+
+  console.table(rows);
+
+  // При завершении работы приложения
+  pool.end();
+})();
