867e8172951972c66fd1b4633f8c37f78246496d
Poly Titova <poly_titova@mail.ru>
2021-04-10 10:23:17 +0300

    Удалили из таблицы одну запись

---
+++ 05-delete.js
@@ -0,0 +1,17 @@
+'use strict';
+
+// подключение пакета
+const {Client} = require(`pg`);
+
+// удаление записи
+(async () => {
+  const connectionString = `postgres://academy:123456@localhost/phonebook`;
+  const client = new Client(connectionString);
+
+  await client.connect();
+
+  const deleteQuery = `DELETE FROM peoples WHERE peoples.email = $1`;
+  const resultDelete = await client.query(deleteQuery, [`test@gr.local`]);
+  console.log(`Удалено записей: ${resultDelete.rowCount}`);
+  client.end();
+})();
